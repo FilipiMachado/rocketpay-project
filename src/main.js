597b9adf8eb19c8c5699793a5f680d285c876fe8
fil <filipisee@hotmail.com>
2022-10-18 15:31:22 -0300

    added more credit card options

--- src/main.js
+++ src/main.js
@@ -6,11 +6,22 @@ const ccBgColor01 = document.querySelector(
 const ccBgColor02 = document.querySelector(
   ".cc-bg svg > g g:nth-child(2) path"
 );
-ccBgColor01.setAttribute("fill", "green");
-ccBgColor02.setAttribute("fill", "blue");
+const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img");
 
-const colors = {
-  visa: ["#436D99", "#2D57F2"],
-  mastercard: ["#DF6F29", "#C69347"],
-  default: ["black", "grey"],
-};
+function setCardType(type) {
+  const colors = {
+    visa: ["#436D99", "#2D57F2"],
+    mastercard: ["#DF6F29", "#C69347"],
+    elo: ["#00A4E0", "#EF4223"],
+    nubank: ["#fff", "#944CB7"],
+    default: ["black", "grey"],
+  };
+
+  ccBgColor01.setAttribute("fill", colors[type][0]);
+  ccBgColor02.setAttribute("fill", colors[type][1]);
+  ccLogo.setAttribute("src", `cc-${type}.svg`);
+}
+
+setCardType("nubank");
+
+globalThis.setCardType = setCardType;
