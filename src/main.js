ad11fe323e912492a74e28708b77a376a3486095
fil <filipisee@hotmail.com>
2022-10-20 19:12:54 -0300

    finished regex

--- src/main.js
+++ src/main.js
@@ -23,7 +23,7 @@ function setCardType(type) {
   ccLogo.setAttribute("src", `cc-${type}.svg`);
 }
 
-setCardType("nubank");
+setCardType("default");
 
 globalThis.setCardType = setCardType;
 
@@ -44,9 +44,48 @@ const expirationDatePattern = {
     },
     YY: {
       mask: IMask.MaskedRange,
-      from: 22,
-      to: 32,
+      from: String(new Date().getFullYear()).slice(2),
+      to: String(new Date().getFullYear() + 10).slice(2),
     },
   },
 };
 const expirationDateMasked = IMask(expirationDate, expirationDatePattern);
+
+const cardNumber = document.querySelector("#card-number");
+const cardNumberPattern = {
+  mask: [
+    {
+      mask: "0000 0000 0000  0000",
+      regex: /^4\d{0,15}/,
+      cardtype: "visa",
+    },
+    {
+      mask: "0000 0000 0000  0000",
+      regex: /^(5[1-5]\d{0,2}|22[2-9]\d{0,1}|2[3-7]\d{0,2})\d{0,12}/,
+      cardtype: "mastercard",
+    },
+    {
+      mask: "0000 0000 0000  0000",
+      cardtype: "default",
+    },
+    /* {
+      mask: "0000 0000 0000  0000",
+      cardtype: "elo"
+    },
+    {
+      mask: "0000 0000 0000  0000",
+      cardtype: "nubank"
+    }, */
+  ],
+  dispatch: function (appended, dynamicMasked) {
+    const number = (dynamicMasked.value + appended).replace(/\D/g, "");
+
+    const foundMask = dynamicMasked.compiledMasks.find(function (item) {
+      return number.match(item.regex);
+    });
+
+    return foundMask;
+  },
+};
+
+const cardNumberMasked = IMask(cardNumber, cardNumberPattern);
